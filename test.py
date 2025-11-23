3348e76b73aeb96a7e434050bb0843c3aaebbf82
Shivy-Joshi <shivyman777@gmail.com>
2025-11-23 01:42:05 -0500

    added pauses between toggle comands

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from pin_toggle import toggle, toggle_forever # your file containing the function
+import time
 
-toggle(27, 3)
+toggle(27, 1)
+time.sleep(1)
 toggle(27,1 )
-toggle(27,3 )
+time.sleep(1)
+toggle(27,1 )
