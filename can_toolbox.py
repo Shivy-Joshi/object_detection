4703b3dc58784b50f3d23693893974e4221d2a8a
Shivy-Joshi <shivyman777@gmail.com>
2025-11-25 03:26:13 -0500

    added the 3 pi CAN commands needed to intiate and start the bus

--- can_toolbox.py
+++ can_toolbox.py
@@ -2,7 +2,39 @@ import time
 import struct
 import can
 
+def setup_can0():
+    """Configure can0 like your shell commands do."""
+    cmds = [
+        # ignore errors when bringing it down (same as '|| true' and redirect)
+        ["ip", "link", "set", "can0", "down"],
+        # set type + bitrate
+        [
+            "ip", "link", "set", "can0",
+            "type", "can",
+            "bitrate", "500000",
+            "sample-point", "0.875",
+            "berr-reporting", "on",
+        ],
+        # bring it up
+        ["ip", "link", "set", "can0", "up"],
+    ]
 
+    # 1) down: ignore failure, hide output
+    subprocess.run(cmds[0],
+                   stdout=subprocess.DEVNULL,
+                   stderr=subprocess.DEVNULL,
+                   check=False)
+
+    # 2) set type/bitrate (fail if this breaks)
+    subprocess.run(cmds[1], check=True)
+
+    # 3) up (fail if this breaks)
+    subprocess.run(cmds[2], check=True)
+
+
+def open_bus():
+    # call this AFTER setup_can0()
+    return can.Bus(interface="socketcan", channel="can0")
 # Create the bus ONCE globally
 bus = can.interface.Bus(channel="can0", bustype="socketcan")
 
